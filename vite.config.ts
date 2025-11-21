1d527c7a868d438e8fac15f0d9d43562ae874f91
leeski <540470452@qq.com>
2025-11-21 10:52:30 +0800

    chore: add local server config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/void-frontend/",
   plugins: [
     vue(),
     AutoImport({
@@ -30,6 +31,45 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
+    },
+  },
+  // 本地后端开发环境配置
+  server: {
+    proxy: {
+      "/open": {
+        target: "http://localhost:12000",
+        rewrite: (path) => {
+          return path.replace(/^\/open/, "");
+        },
+        //这里改变的是服务器的源,服务器上的ctx.host是3000
+        changeOrigin: true,
+      },
+      "/proxy": {
+        target: "https://edb.cie-cn.com:8066",
+        rewrite: (path) => {
+          return path.replace(/^\/proxy/, "");
+        },
+        changeOrigin: true,
+      },
+      "/old": {
+        target: "https://yus.cie-cn.com:7080",
+        rewrite: (path) => {
+          return path.replace(/^\/old/, "");
+        },
+        changeOrigin: true,
+      },
+      "/v1": {
+        target: "https://api.dify.ai",
+        rewrite: (path) => {
+          return path.replace(/^\/v1/, "/v1");
+        },
+        changeOrigin: true,
+      },
+      "/napi": {
+        target: "http://192.168.48.132:7777", // Flask 运行地址
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/napi/, ""), // 转发为 Flask 路由
+      },
     },
   },
 });
